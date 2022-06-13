d81b798ca08f6bb801842ac996cc54eb33b83eec
Kelsey <k.m.beck92@gmail.com>
2022-06-13 08:16:25 -0400

    more work on transaction/transfer classes

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransactionDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransactionDao.java
@@ -2,16 +2,14 @@ package com.techelevator.tenmo.dao;
 
 import com.techelevator.tenmo.model.Transaction;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface TransactionDao {
 
-    List<Transaction> findAll();
+    List<Transaction> findAllUserTransactions();
 
-    Transaction findByUsername(String username);
+    Transaction findTransactionByTransferId(Long transferId) throws Exception;
 
-    int findIdByUsername(String username);
-
-    boolean create(String username, String password);
-    Transaction findByUserById(Long id);
+    boolean create(long transferTypeId, long transferStatusId, long accountFrom, long accountTo, BigDecimal amount);
 }
